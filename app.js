c7b00664c850c18a37399e86bcdbbf0fd061a319
DelicZijad <108204272+DelicZijad@users.noreply.github.com>
2022-10-14 19:07:39 +0200

    update

--- app.js
+++ app.js
@@ -15,22 +15,6 @@ const arrows=document.getElementsByClassName('content--arrow');
 [...arrows].forEach(arr=>arr.classList.remove('upside'))
  const arrow=e.target.querySelector('.content--arrow')
     arrow.classList.add('upside')
-
-    
-   if(this.classList.contains('clicked')){
-    arrow.classList.remove('upside')
-    answer.classList.add('hide')
-    this.classList.add('unclicked')
-    
-   }
- 
-
-   this.classList.add('clicked')
-       if(this.classList.contains('unclicked')){
-      this.classList.remove('clicked')
-      this.classList.remove('unclicked')
-    }
-   console.log(this);
     })
 
 })
